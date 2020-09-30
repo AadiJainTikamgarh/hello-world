dbec48a29dcf6f01fcce7160b46c545ba8c66528
Aadi Jain <68893949+AadiJainTikamgarh@users.noreply.github.com>
2020-09-30 05:45:30 -0700

    Update main code.py

--- main code.py
+++ main code.py
@@ -1,13 +1,18 @@
 import random
 
+# decide the range of random number
 secret = random.randrange(1, 101)
 
-guess = 0
-tries = 0
+# vaiables 
+guess = 0 # Storing a guesses
+tries = 0 # counting no of guesses
+
+#running a loop for running our game continously
 while guess != secret:
     guess = int(input("Make a guess: "))
     tries = tries + 1
     
+    # checking the guesses
     if guess > secret:
         print("Too High!")
     elif guess < secret:
@@ -15,4 +20,5 @@ while guess != secret:
     else:
         print("You got it!")
 
+# printing the number of tries
 print("Number of tries:", tries)
